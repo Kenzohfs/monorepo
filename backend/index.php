f8c1e79348ee4afbdc2c42b6906712b6d39cb148
Kenzo Sato <kenzohideaky@gmail.com>
2026-01-02 16:41:27 -0300

    test: sonar

--- backend/index.php
+++ backend/index.php
@@ -66,3 +66,67 @@ function processData($data) {
   
   return $result;
 }
+
+// Complex function with nested conditions
+function validateOrder($order, $user, $inventory) {
+  if ($order == null) {
+    return false;
+  }
+  
+  if ($user == null) {
+    return false;
+  }
+  
+  if (!isset($order['items']) || count($order['items']) == 0) {
+    return false;
+  }
+  
+  $total = 0;
+  foreach ($order['items'] as $item) {
+    if (!isset($item['id']) || !isset($item['quantity'])) {
+      return false;
+    }
+    
+    if ($item['quantity'] <= 0) {
+      return false;
+    }
+    
+    if (!isset($inventory[$item['id']])) {
+      return false;
+    }
+    
+    if ($inventory[$item['id']]['stock'] < $item['quantity']) {
+      return false;
+    }
+    
+    $price = $inventory[$item['id']]['price'];
+    $total += $price * $item['quantity'];
+  }
+  
+  if (isset($order['discount'])) {
+    if ($order['discount'] > 0 && $order['discount'] < 100) {
+      $total = $total * (1 - $order['discount'] / 100);
+    }
+  }
+  
+  return $total > 0;
+}
+
+// Function with magic numbers
+function calculateShipping($weight, $distance) {
+  if ($weight < 1) {
+    return 5.00;
+  } elseif ($weight < 5) {
+    return 10.00;
+  } elseif ($weight < 10) {
+    return 15.00;
+  } else {
+    return 20.00 + ($distance * 0.5);
+  }
+}
+
+// Another unused function
+function helperFunction($x, $y, $z) {
+  $temp = $x + $y;
+  return $temp * $z;
+}
